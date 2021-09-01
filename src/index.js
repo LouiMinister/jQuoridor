349eac88fe485ad8f560f1c0ce9a7157d6825e61
YJ <kkk287150155@naver.com>
2021-09-01 23:46:25 +0900

    feature 셀

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  renderSquare(x_coords, y_coords) {
+    return <Square />;
+  }
+
+  render() {
+    const x_coords = new Array(9).fill(null).map((_, idx) => {
+      return idx;
+    });
+    const y_coords = new Array(9).fill(null).map((_, idx) => {
+      return idx;
+    });
+
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        {y_coords.map((_, y_idx) => {
+          return (
+            <div className="board-row">
+              {x_coords.map((_, x_idx) => {
+                return this.renderSquare(x_idx, y_idx);
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
