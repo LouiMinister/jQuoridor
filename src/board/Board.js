6489eae2cbcfefdaa410cd6bb10707ca149dc0fa
LouiMinister <LouiMinister@gmail.com>
2021-09-02 21:52:58 +0900

    refactor: Board 코드 정리

--- src/board/Board.js
+++ src/board/Board.js
@@ -5,8 +5,8 @@ function Board({ width, height }) {
 
   const boardStyle = {
     display: "grid",
-    gridTemplateColumns: "64px repeat(8, 32px 64px)",
-    gridTemplateRows: "64px repeat(8, 32px 64px)",
+    gridTemplateColumns: `64px repeat(${width - 1}, 32px 64px)`,
+    gridTemplateRows: `64px repeat(${height - 1}, 32px 64px)`,
   }
 
   return (
@@ -15,9 +15,11 @@ function Board({ width, height }) {
         new Array(height * 2 - 1).fill(null).map((_, columnIdx) => {
           return new Array(width * 2 - 1).fill(null).map((_, rowIdx) => {
             if (columnIdx % 2 === 0 && rowIdx % 2 === 0) {
-              return <Cell text={`${Math.floor(columnIdx / 2)}:${Math.floor(rowIdx / 2)}`} />
+              const key = `${Math.floor(columnIdx / 2)}:${Math.floor(rowIdx / 2)}`;
+              return <Cell text={key} key={key} />
             } else {
-              return (<Gutter text={`${(columnIdx) / 2}:${(rowIdx) / 2}`} />)
+              const key = `${(columnIdx) / 2}:${(rowIdx) / 2}`
+              return (<Gutter text={key} key={key} />)
             }
           })
         })
