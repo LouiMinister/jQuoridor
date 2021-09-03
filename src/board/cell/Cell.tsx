93225d068edf8d0dd6f97bea7c6648ff36143af9
LouiMinister <louiminister@gmail.com>
2021-09-03 22:15:45 +0900

    chore: Migration js files to typescript

--- src/board/cell/Cell.tsx
+++ src/board/cell/Cell.tsx
@@ -1,5 +1,8 @@
-function Cell({ text, onMouseOver, status }) {
 
+import { SpaceStatus } from './../BoardMap';
+
+function Cell({ text, onMouseOver, status }:
+  { text: string, onMouseOver: () => void, status: SpaceStatus }) {
 
   return (
     <div style={{ display: "flex", width: "100%", height: "100%", backgroundColor: 'mediumseagreen', borderStyle: "solid", borderWidth: "2px", borderColor: 'WindowFrame', justifyContent: "center", alignItems: "center" }}
