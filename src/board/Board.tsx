8362449bd44f7a6ca70838fee1ef31dda36c5980
LouiMinister <louiminister@gmail.com>
2021-09-04 21:08:22 +0900

    refactor: Board.tsx 최적화

--- src/board/Board.tsx
+++ src/board/Board.tsx
@@ -1,41 +1,67 @@
 import _ from 'lodash';
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import { BoardMap } from "./BoardMap";
 import Cell from "./cell/Cell";
 import { Coord } from "./Coord";
 import Gutter from "./gutter/Gutter";
+import { ObstacleDirection } from './Obstacle';
+
+function boardMapReducer(boardMap: BoardMap, action): BoardMap {
+  switch (action.type) {
+    case 'BUILD_OBSTACLE':
+      boardMap.buildObstacle(new Coord(action.x, action.y), action.obstacleDirectionMode);
+      break;
+    case 'BUILD_PRE_OBSTACLE':
+      boardMap.clearPreObstacle();
+      boardMap.buildPreObstacle(new Coord(action.x, action.y), action.obstacleDirectionMode);
+      console.log('BUILD_PRE_OBSTACLE', action);
+      break;
+  }
+  return _.cloneDeep(boardMap);
+}
 
 function Board({ width, height }:
   { width: number, height: number }) {
 
   const [mouseCoord, setMouseCoord] = useState({ x: 0, y: 0 });
-  const [boardMap, setBoardMap] = useState(new BoardMap(width, height));
+  const [obstacleDirectionMode, setObstacleDirectionMode] = useState('horizontal' as ObstacleDirection)
+  const [boardMap, boardMapDispatch] = useReducer(boardMapReducer, new BoardMap(width, height));
 
-  const onClickGutter = () => {
-    boardMap.buildObstacle(new Coord(mouseCoord.x, mouseCoord.y), 'horizontal');
-    setBoardMap(_.cloneDeep(boardMap));
-  }
+  useEffect(() => {
+    boardMapDispatch({ type: 'BUILD_PRE_OBSTACLE', x: mouseCoord.x, y: mouseCoord.y, obstacleDirectionMode });
+  }, [mouseCoord, obstacleDirectionMode])
 
-  const onMouseOver = (coord) => {
+  const onClickGutter = useCallback(() => {
+    boardMapDispatch({ type: 'BUILD_OBSTACLE', x: mouseCoord.x, y: mouseCoord.y, obstacleDirectionMode });
+  }, [mouseCoord, obstacleDirectionMode]);
+
+  const onMouseOver = useCallback((coord) => {
     const { x, y } = coord;
-    boardMap.clearPreObstacle();
-    boardMap.buildPreObstacle(new Coord(x, y), 'horizontal');
-    setBoardMap(_.cloneDeep(boardMap));
     setMouseCoord({ x, y });
-  }
+  }, [setMouseCoord]);
 
-  const boardStyle = {
-    display: "grid",
-    gridTemplateColumns: `64px repeat(${width - 1}, 32px 64px)`,
-    gridTemplateRows: `64px repeat(${height - 1}, 32px 64px)`,
-  }
+  const switchObstacleDirectionMode = useCallback(() => {
+    if (obstacleDirectionMode === 'horizontal') {
+      setObstacleDirectionMode('vertical');
+    } else {
+      setObstacleDirectionMode('horizontal');
+    }
+  }, [obstacleDirectionMode])
+
+  const boardStyle = useMemo(() => {
+    return {
+      display: "grid",
+      gridTemplateColumns: `64px repeat(${width - 1}, 32px 64px)`,
+      gridTemplateRows: `64px repeat(${height - 1}, 32px 64px)`,
+    }
+  }, [width, height])
 
   return (
     <>
       <div>
         {`xCoord: ${mouseCoord.x} yCoord: ${mouseCoord.y}`}
       </div>
-      <div style={boardStyle}>
+      <div style={boardStyle} onContextMenu={(e) => { e.preventDefault(); switchObstacleDirectionMode(); }}>
         {
           boardMap.getSpaceToAry(({ coord, space }) => {
             if (Number.isInteger(coord.x) && Number.isInteger(coord.y)) {
