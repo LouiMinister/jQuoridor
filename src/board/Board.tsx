7cacf6b64be00559b12547257ce2e116b7551f30
YJ <kkk287150155@naver.com>
2021-09-06 18:23:00 +0900

    feature Board 중앙정렬, 말 대기석 모양만 추가

--- src/board/Board.tsx
+++ src/board/Board.tsx
@@ -1,77 +1,166 @@
-import _ from 'lodash';
+import _ from "lodash";
 import { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import { BoardMap } from "./BoardMap";
 import Cell from "./cell/Cell";
 import { Coord } from "./Coord";
 import Gutter from "./gutter/Gutter";
-import { ObstacleDirection } from './Obstacle';
+import { ObstacleDirection } from "./Obstacle";
+import Waiting from "./waiting/waiting";
 
 function boardMapReducer(boardMap: BoardMap, action): BoardMap {
   switch (action.type) {
-    case 'BUILD_OBSTACLE':
-      boardMap.buildObstacle(new Coord(action.x, action.y), action.obstacleDirectionMode, action.playerTurn);
+    case "BUILD_OBSTACLE":
+      boardMap.buildObstacle(
+        new Coord(action.x, action.y),
+        action.obstacleDirectionMode,
+        action.playerTurn
+      );
       break;
-    case 'BUILD_PRE_OBSTACLE':
+    case "BUILD_PRE_OBSTACLE":
       boardMap.clearPreObstacle();
-      boardMap.buildPreObstacle(new Coord(action.x, action.y), action.obstacleDirectionMode, action.playerTurn);
-      console.log('BUILD_PRE_OBSTACLE', action);
+      boardMap.buildPreObstacle(
+        new Coord(action.x, action.y),
+        action.obstacleDirectionMode,
+        action.playerTurn
+      );
+      console.log("BUILD_PRE_OBSTACLE", action);
       break;
   }
   return _.cloneDeep(boardMap);
 }
 
-function Board({ width, height }:
-  { width: number, height: number }) {
-
+function Board({
+  width,
+  height,
+  marker_max,
+}: {
+  width: number;
+  height: number;
+  marker_max: number;
+}) {
   const [mouseCoord, setMouseCoord] = useState({ x: 0, y: 0 });
-  const [obstacleDirectionMode, setObstacleDirectionMode] = useState('horizontal' as ObstacleDirection)
-  const [boardMap, boardMapDispatch] = useReducer(boardMapReducer, new BoardMap(width, height));
-  const [playerTurn, setPlayerTurn] = useState('home');
+  const [obstacleDirectionMode, setObstacleDirectionMode] = useState(
+    "horizontal" as ObstacleDirection
+  );
+  const [boardMap, boardMapDispatch] = useReducer(
+    boardMapReducer,
+    new BoardMap(width, height)
+  );
+  const [playerTurn, setPlayerTurn] = useState("home");
 
   useEffect(() => {
-    boardMapDispatch({ type: 'BUILD_PRE_OBSTACLE', x: mouseCoord.x, y: mouseCoord.y, obstacleDirectionMode, playerTurn });
-  }, [mouseCoord, obstacleDirectionMode, playerTurn])
-
-  const onClickGutter = useCallback(() => {
-    boardMapDispatch({ type: 'BUILD_OBSTACLE', x: mouseCoord.x, y: mouseCoord.y, obstacleDirectionMode, playerTurn });
+    boardMapDispatch({
+      type: "BUILD_PRE_OBSTACLE",
+      x: mouseCoord.x,
+      y: mouseCoord.y,
+      obstacleDirectionMode,
+      playerTurn,
+    });
   }, [mouseCoord, obstacleDirectionMode, playerTurn]);
 
-  const onMouseOver = useCallback((coord) => {
-    const { x, y } = coord;
-    setMouseCoord({ x, y });
-  }, [setMouseCoord]);
+  const onClickGutter = useCallback(() => {
+    boardMapDispatch({
+      type: "BUILD_OBSTACLE",
+      x: mouseCoord.x,
+      y: mouseCoord.y,
+      obstacleDirectionMode,
+      playerTurn,
+    });
+  }, [mouseCoord, obstacleDirectionMode, playerTurn]);
+
+  const onMouseOver = useCallback(
+    (coord) => {
+      const { x, y } = coord;
+      setMouseCoord({ x, y });
+    },
+    [setMouseCoord]
+  );
 
   const switchObstacleDirectionMode = useCallback(() => {
-    if (obstacleDirectionMode === 'horizontal') {
-      setObstacleDirectionMode('vertical');
+    if (obstacleDirectionMode === "horizontal") {
+      setObstacleDirectionMode("vertical");
     } else {
-      setObstacleDirectionMode('horizontal');
+      setObstacleDirectionMode("horizontal");
     }
-  }, [obstacleDirectionMode])
+  }, [obstacleDirectionMode]);
 
   const boardStyle = useMemo(() => {
     return {
+      justifyContent: "center",
       display: "grid",
       gridTemplateColumns: `64px repeat(${width - 1}, 32px 64px)`,
       gridTemplateRows: `64px repeat(${height - 1}, 32px 64px)`,
-    }
-  }, [width, height])
+    };
+  }, [width, height]);
+
+  const waitingStyle = useMemo(() => {
+    return {
+      marginBottom: "10px",
+      marginTop: "10px",
+      justifyContent: "center",
+      display: "grid",
+      gridTemplateColumns: `40px repeat(${marker_max - 1}, 45px 40px)`,
+    };
+  }, [marker_max]);
 
   return (
     <>
-      <div>
-        {`xCoord: ${mouseCoord.x} yCoord: ${mouseCoord.y}`}
+      <div>{`xCoord: ${mouseCoord.x} yCoord: ${mouseCoord.y}`}</div>
+
+      {/* 말 대기중 */}
+      <div style={waitingStyle}>
+        {Array.from({ length: marker_max * 2 - 1 }, (v, i) => i).map((i) => {
+          if (i % 2 === 0) {
+            let key = `${i}:home`;
+            return <Waiting key={key}></Waiting>;
+          } else {
+            return <div></div>;
+          }
+        })}
       </div>
-      <div style={boardStyle} onContextMenu={(e) => { e.preventDefault(); switchObstacleDirectionMode(); }}>
-        {
-          boardMap.getSpaceToAry(({ coord, space }) => {
-            if (Number.isInteger(coord.x) && Number.isInteger(coord.y)) {
-              return <Cell text={coord.toKey()} key={coord.toKey()} onMouseOver={() => onMouseOver(coord)} status={space.status} owner={space.owner} />
-            } else {
-              return <Gutter text={coord.toKey()} key={coord.toKey()} onMouseOver={() => onMouseOver(coord)} onClick={() => onClickGutter()} status={space.status} />
-            }
-          })
-        }
+
+      <div
+        style={boardStyle}
+        onContextMenu={(e) => {
+          e.preventDefault();
+          switchObstacleDirectionMode();
+        }}
+      >
+        {boardMap.getSpaceToAry(({ coord, space }) => {
+          if (Number.isInteger(coord.x) && Number.isInteger(coord.y)) {
+            return (
+              <Cell
+                text={coord.toKey()}
+                key={coord.toKey()}
+                onMouseOver={() => onMouseOver(coord)}
+                status={space.status}
+                owner={space.owner}
+              />
+            );
+          } else {
+            return (
+              <Gutter
+                text={coord.toKey()}
+                key={coord.toKey()}
+                onMouseOver={() => onMouseOver(coord)}
+                onClick={() => onClickGutter()}
+                status={space.status}
+              />
+            );
+          }
+        })}
+        {/* 말 대기중 */}
+      </div>
+
+      <div style={waitingStyle}>
+        {Array.from({ length: marker_max * 2 - 1 }, (v, i) => i).map((i) => {
+          if (i % 2 === 0) {
+            let key = `${i}:home`;
+            return <Waiting key={key}></Waiting>;
+          } else {
+            return <div></div>;
+          }
+        })}
       </div>
     </>
   );
---
+++ src/board/waiting/waiting.tsx
@@ -0,0 +1,20 @@
+function Waiting() {
+  const renderWaiting = () => {
+    // 보드 크기 832x832
+    return (
+      <div
+        style={{
+          height: "120px",
+          borderRadius: "10%",
+          backgroundColor: "#82755b",
+          borderWidth: "2px",
+          borderStyle: "solid",
+        }}
+      ></div>
+    );
+  };
+
+  return <>{renderWaiting()}</>;
+}
+
+export default Waiting;
