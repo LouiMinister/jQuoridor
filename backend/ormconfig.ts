579899bea68d70a9ccec5fbeb7a1512b7cc16085
LouiMinister <louiminister@gmail.com>
2021-09-11 18:10:31 +0900

    feature: TypeORM 추가

---
+++ backend/ormconfig.ts
@@ -0,0 +1,27 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { Users } from './src/entities/Users';
+
+dotenv.config();
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [
+    Users,
+  ],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  cli: { migrationsDir: 'src/migrations' },
+  autoLoadEntities: true,
+  charset: 'utf8mb4',
+  synchronize: true,
+  logging: true,
+  keepConnectionAlive: true,
+};
+
+console.log(process.env.DB_USERNAME);
+export = config;
+
