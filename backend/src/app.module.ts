fcbecb94e94c8caeaa4758946664aae90dc7aab2
LouiMinister <louiminister@gmail.com>
2021-09-11 15:56:10 +0900

    feature: create nest gateway (socket)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
