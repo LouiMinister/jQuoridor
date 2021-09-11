603628a78ed1f006ef6ca27f871ba54d31a55158
LouiMinister <louiminister@gmail.com>
2021-09-11 19:26:03 +0900

    chore: [back] fix orm config

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -17,7 +17,7 @@ const config: TypeOrmModuleOptions = {
   cli: { migrationsDir: 'src/migrations' },
   autoLoadEntities: true,
   charset: 'utf8mb4',
-  synchronize: true,
+  synchronize: false,
   logging: true,
   keepConnectionAlive: true,
 };
